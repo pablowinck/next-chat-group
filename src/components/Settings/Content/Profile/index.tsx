73cf74913be8dae01ede19b7f11b124c6ae62a83
João Pedro <joaopsilvamagalhaes@gmail.com>
2022-03-15 19:27:42 -0300

    feat: add toast component

--- src/components/Settings/Content/Profile/index.tsx
+++ src/components/Settings/Content/Profile/index.tsx
@@ -1,218 +1,200 @@
-import ModalMessage from 'components/ModalMessage';
+import { useToast } from 'components/Toast';
 import { useUserContext } from 'contexts/UserContext';
 import React, { useState } from 'react';
 import api from 'utils/api';
 import {
-    Avatar,
-    CancelButton,
-    ChangePassword,
-    Container,
-    Content,
-    Error,
-    Footer,
-    Form,
-    GroupButton,
-    Input,
-    Label,
-    LabelTitle,
-    RedButton,
-    SaveButton,
-    Title
+   Avatar,
+   CancelButton,
+   ChangePassword,
+   Container,
+   Content,
+   Error,
+   Footer,
+   Form,
+   GroupButton,
+   Input,
+   Label,
+   LabelTitle,
+   RedButton,
+   SaveButton,
+   Title
 } from './styles';
 
 const ProfileContent: React.FC = () => {
-    const { user, setUser, setLogged } = useUserContext();
-    const [name, setName] = useState(user.name);
-    const [email, setEmail] = useState(user.email);
-    const [password, setPassword] = useState('');
-    const [success, setSuccess] = useState(false);
-    const [error, setError] = useState(false);
-    const [errors, setErrors] = useState({
-        name: '',
-        email: '',
-        password: ''
-    });
-    const [loading, setLoading] = useState(false);
+   const { user, setUser, setLogged } = useUserContext();
+   const [name, setName] = useState(user.name);
+   const [email, setEmail] = useState(user.email);
+   const [password, setPassword] = useState('');
+   const [errors, setErrors] = useState({
+      name: '',
+      email: '',
+      password: ''
+   });
+   const [loading, setLoading] = useState(false);
+   const toast = useToast();
 
-    const resetErrors = () => {
-        setErrors({
-            name: '',
-            email: '',
-            password: ''
-        });
-    };
+   const resetErrors = () => {
+      setErrors({
+         name: '',
+         email: '',
+         password: ''
+      });
+   };
 
-    const handleChange = (
-        value: string,
-        set: (value: string) => void,
-        type: string
-    ) => {
-        if (!value || /^\s*$/.test(value)) {
-            setErrors({
-                ...errors,
-                [type]: 'required field'
-            });
-        } else if (value.length < 4) {
-            setErrors({
-                ...errors,
-                [type]: 'must be at least 4 characters'
-            });
-        } else {
-            setErrors({
-                ...errors,
-                [type]: ''
-            });
-        }
-        set(value);
-    };
+   const handleChange = (
+      value: string,
+      set: (value: string) => void,
+      type: string
+   ) => {
+      if (!value || /^\s*$/.test(value)) {
+         setErrors({
+            ...errors,
+            [type]: 'required field'
+         });
+      } else if (value.length < 4) {
+         setErrors({
+            ...errors,
+            [type]: 'must be at least 4 characters'
+         });
+      } else {
+         setErrors({
+            ...errors,
+            [type]: ''
+         });
+      }
+      set(value);
+   };
 
-    const handleSubmit = async () => {
-        if (!password) {
-            setErrors({
-                ...errors,
-                ['password']: 'required field'
-            });
-            return;
-        }
+   const handleSubmit = async () => {
+      if (!password) {
+         setErrors({
+            ...errors,
+            ['password']: 'required field'
+         });
+         return;
+      }
 
-        setLoading(true);
-        await api
-            .patch(
-                'users/' + user.id,
-                JSON.stringify({
-                    name: name,
-                    email: email,
-                    password: password,
-                    profileImage: user.profileImage
-                }),
-                {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
-            )
-            .then((res) => {
-                setUser(res.data);
-                localStorage.setItem('user', JSON.stringify(res.data));
-                resetErrors();
-                setSuccess(true);
-            })
-            .catch((err) => {
-                console.log(err);
-                setError(true);
-            })
-            .finally(() => {
-                setLoading(false);
-            });
-    };
+      setLoading(true);
+      await api
+         .patch(
+            'users/' + user.id,
+            JSON.stringify({
+               name: name,
+               email: email,
+               password: password,
+               profileImage: user.profileImage
+            }),
+            {
+               headers: {
+                  'Content-Type': 'application/json'
+               }
+            }
+         )
+         .then((res) => {
+            setUser(res.data);
+            localStorage.setItem('user', JSON.stringify(res.data));
+            resetErrors();
 
-    const handleDisableAcount = async () => {
-        setLoading(true);
-        await api
-            .post('users/disable/' + user.id, {
-                password
-            })
-            .then((_) => {
-                localStorage.removeItem('user');
-                setSuccess(true);
-                setTimeout(() => {
-                    setLogged(false);
-                }, 3000);
-            })
-            .catch((_) => {
-                setError(true);
-            })
-            .finally(() => {
-                setLoading(false);
-            });
-    };
+            toast.success('Success');
+         })
+         .catch((err) => {
+            console.log(err);
+            toast.error('Incorrect password');
+         })
+         .finally(() => {
+            setLoading(false);
+         });
+   };
 
-    return (
-        <Container>
-            <Title>My Profile</Title>
+   const handleDisableAcount = async () => {
+      setLoading(true);
+      await api
+         .post('users/disable/' + user.id, {
+            password
+         })
+         .then((_) => {
+            localStorage.removeItem('user');
+            toast.success('Success');
+            setTimeout(() => {
+               setLogged(false);
+            }, 3000);
+         })
+         .catch((_) => {
+            toast.error('Incorrect password');
+         })
+         .finally(() => {
+            setLoading(false);
+         });
+   };
 
-            <Content>
-                <Avatar image={user.profileImage} />
+   return (
+      <Container>
+         <Title>My Profile</Title>
+         <Content>
+            <Avatar image={user.profileImage} />
 
-                <Form
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                    }}
-                >
-                    <LabelTitle>
-                        <Label className="mt-0">Name</Label>
-                        {errors.name && <Error>{errors.name}</Error>}
-                    </LabelTitle>
-                    <Input
-                        name="name"
-                        placeholder="Name"
-                        value={name}
-                        onChange={(e) =>
-                            handleChange(e.target.value, setName, 'name')
-                        }
-                    />
+            <Form
+               onSubmit={(e) => {
+                  e.preventDefault();
+               }}
+            >
+               <LabelTitle>
+                  <Label className="mt-0">Name</Label>
+                  {errors.name && <Error>{errors.name}</Error>}
+               </LabelTitle>
+               <Input
+                  name="name"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) =>
+                     handleChange(e.target.value, setName, 'name')
+                  }
+               />
 
-                    <LabelTitle>
-                        <Label>Email</Label>
-                        {errors.email && <Error>{errors.email}</Error>}
-                    </LabelTitle>
+               <LabelTitle>
+                  <Label>Email</Label>
+                  {errors.email && <Error>{errors.email}</Error>}
+               </LabelTitle>
 
-                    <Input
-                        name="email"
-                        placeholder="E-mail"
-                        value={email}
-                        onChange={(e) =>
-                            handleChange(e.target.value, setEmail, 'email')
-                        }
-                        type="email"
-                    />
-                    <LabelTitle>
-                        <Label>Current Password</Label>
-                        {errors.password !== '' && (
-                            <Error>{errors.password}</Error>
-                        )}
-                    </LabelTitle>
-                    <Input
-                        name="password"
-                        placeholder="Current Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        type="password"
-                    />
+               <Input
+                  name="email"
+                  placeholder="E-mail"
+                  value={email}
+                  onChange={(e) =>
+                     handleChange(e.target.value, setEmail, 'email')
+                  }
+                  type="email"
+               />
+               <LabelTitle>
+                  <Label>Current Password</Label>
+                  {errors.password !== '' && <Error>{errors.password}</Error>}
+               </LabelTitle>
+               <Input
+                  name="password"
+                  placeholder="Current Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+               />
 
-                    <ChangePassword>Change Password?</ChangePassword>
-                </Form>
-            </Content>
-
-            <Footer>
-                <GroupButton>
-                    <RedButton onClick={handleDisableAcount}>
-                        Disable Account
-                    </RedButton>
-                </GroupButton>
-                <GroupButton>
-                    <CancelButton>Cancel</CancelButton>
-                    <SaveButton onClick={handleSubmit}>
-                        {loading ? 'Loading...' : 'Save'}
-                    </SaveButton>
-                </GroupButton>
-            </Footer>
-            {success && (
-                <ModalMessage
-                    open={success}
-                    setOpen={setSuccess}
-                    type="success"
-                >
-                    Success
-                </ModalMessage>
-            )}
-            {error && (
-                <ModalMessage open={error} setOpen={setError} type="error">
-                    {'Incorrect password'}
-                </ModalMessage>
-            )}
-        </Container>
-    );
+               <ChangePassword>Change Password?</ChangePassword>
+            </Form>
+         </Content>
+         <Footer>
+            <GroupButton>
+               <RedButton onClick={handleDisableAcount}>
+                  Disable Account
+               </RedButton>
+            </GroupButton>
+            <GroupButton>
+               <CancelButton>Cancel</CancelButton>
+               <SaveButton onClick={handleSubmit}>
+                  {loading ? 'Loading...' : 'Save'}
+               </SaveButton>
+            </GroupButton>
+         </Footer>
+      </Container>
+   );
 };
 
 export default ProfileContent;
