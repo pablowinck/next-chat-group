38c15b5af14c7cf82d80df9e9b627d8b9577a5bb
pablowinck <pablowinck123@gmail.com>
2022-03-26 11:39:35 -0300

    add blur in overlay

--- src/components/Overlay/index.tsx
+++ src/components/Overlay/index.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 import { Container } from './styles';
 
 interface IProps {
-    onClick: () => void;
+   onClick: () => void;
 }
 
 const Overlay: React.FC<IProps> = ({ onClick }) => {
-    return <Container onClick={onClick} />;
+   return (
+      <Container
+         onClick={onClick}
+         initial={{
+            opacity: 0
+         }}
+         animate={{
+            opacity: 1
+         }}
+         transition={{
+            duration: 0.5
+         }}
+      />
+   );
 };
 
 export default Overlay;
--- src/components/Overlay/styles.ts
+++ src/components/Overlay/styles.ts
@@ -1,9 +1,12 @@
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    position: fixed;
-    inset: 0;
-
-    background-color: ${(props) => props.theme.colors.background[800]};
-    opacity: 0.5;
+export const Container = styled(motion.div)`
+   position: fixed;
+   width: 100%;
+   height: 100%;
+   top: 0;
+   left: 0;
+   background: rgba(0, 0, 0, 0.5);
+   backdrop-filter: blur(5px);
 `;
